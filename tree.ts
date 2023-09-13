18de8c1d1b0b9b55695a8cedf937277f03984e9d
Aymen <joewilliamdd31@gmail.com>
2023-09-13 21:36:49 +0100

    Create Node class

--- tree.ts
+++ tree.ts
@@ -1,1 +1,11 @@
 export type Spot = { x: number; y: number };
+
+class Node {
+    data: Spot;
+    nextPossibleMoves: Spot[] | null;
+
+    constructor(data: Spot) {
+        this.data = data;
+        this.nextPossibleMoves = null;
+    }
+}
