7ddbe08d682de9deb2674429e636b7985e2e13d1
Aymen <joewilliamdd31@gmail.com>
2023-09-17 08:28:12 +0100

    Rewrite the entire project

--- main.ts
+++ main.ts
@@ -1,16 +1,1 @@
-import { Spot } from "./tree.ts";
 
-function generateChessBoard(): Spot[] {
-    const board: Spot[] = [];
-    const numbers = [0, 1, 2, 3, 4, 5, 6, 7];
-
-    for (const x of numbers) {
-        for (const y of numbers) {
-            board.push(new Spot(x, y));
-        }
-    }
-
-    return board;
-}
-
-const board = generateChessBoard();
