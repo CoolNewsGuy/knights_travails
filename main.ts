4ece11ea7e21c13657f9084d642fe80ef600899c
Aymen <joewilliamdd31@gmail.com>
2023-09-22 14:16:23 +0100

    Create isOutOfBoard method

--- main.ts
+++ main.ts
@@ -10,4 +10,8 @@ class Spot {
     isEqualTo(other: Spot): boolean {
         return this.x === other.x && this.y === other.y;
     }
+
+    isOutOfBoard(): boolean {
+        return this.x > 7 || this.y > 7 || this.x < 0 || this.y < 0;
+    }
 }
